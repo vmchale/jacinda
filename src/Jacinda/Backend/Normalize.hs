844834cdbd9f154806db2dee307c13cfb08e3b2b
Vanessa McHale <vamchale@gmail.com>
2022-01-03 14:21:15 -0600

    not on norm

--- src/Jacinda/Backend/Normalize.hs
+++ src/Jacinda/Backend/Normalize.hs
@@ -3,7 +3,7 @@ module Jacinda.Backend.Normalize ( eNorm
                                  , compileR
                                  ) where
 
-import           Control.Recursion (cata, cataM, embed)
+import           Control.Recursion (cata, embed)
 import qualified Data.ByteString   as BS
 import           Jacinda.AST
 import           Jacinda.Regex
@@ -19,118 +19,160 @@ compileR = cata a where
 -- will need a state/context at some point (let &c.)
 eNorm :: E (T K)
       -> E (T K)
-eNorm = cata a where
-    a e@FieldF{}       = embed e
-    a e@IParseFieldF{} = embed e
-    a e@FParseFieldF{} = embed e
-    a e@IntLitF{}      = embed e
-    a e@FloatLitF{}    = embed e
-    a e@BoolLitF{}     = embed e
-    a e@StrLitF{}      = embed e
-    a e@RegexLitF{}    = embed e
-    a e@RegexCompiledF{} = embed e
-    a e@UBuiltinF{}    = embed e
-    a e@ColumnF{}      = embed e
-    a e@AllColumnF{}   = embed e
-    a e@IParseColF{}   = embed e
-    a e@FParseColF{}   = embed e
-    a e@AllFieldF{}    = embed e
-    a e@GuardedF{}     = embed e
-    a e@LamF{}         = embed e
-    a e@BBuiltinF{}    = embed e
-    a e@TBuiltinF{}    = embed e
-    a e@TupF{}         = embed e
-    a e@IxF{}          = embed e
-    a e@(EAppF _ BBuiltin{} _) = embed e
-    a e0@(EAppF _ (EApp _ (BBuiltin _ Matches) e) e') =
-        case (e, e') of
-            (RegexCompiled re, StrLit _ str) -> BoolLit tyBool (isMatch' re str)
-            (StrLit _ str, RegexCompiled re) -> BoolLit tyBool (isMatch' re str)
-            _                                -> embed e0
-    a e0@(EAppF _ (EApp _ (BBuiltin _ NotMatches) e) e') =
-        case (e, e') of
-            (RegexCompiled re, StrLit _ str) -> BoolLit tyBool (not $ isMatch' re str)
-            (StrLit _ str, RegexCompiled re) -> BoolLit tyBool (not $ isMatch' re str)
-            _                                -> embed e0
-    a e0@(EAppF _ (EApp _ (BBuiltin (TyArr _ (TyB _ TyInteger) _) Plus) e) e') =
-        case (e, e') of
-            (IntLit _ i, IntLit _ j) -> IntLit tyI (i+j)
-            _                        -> embed e0
-    a e0@(EAppF _ (EApp _ (BBuiltin (TyArr _ (TyB _ TyInteger) _) Max) e) e') =
-        case (e, e') of
-            (IntLit _ i, IntLit _ j) -> IntLit tyI (max i j)
-            _                        -> embed e0
-    a e0@(EAppF _ (EApp _ (BBuiltin (TyArr _ (TyB _ TyInteger) _) Min) e) e') =
-        case (e, e') of
-            (IntLit _ i, IntLit _ j) -> IntLit tyI (min i j)
-            _                        -> embed e0
-    a e0@(EAppF _ (EApp _ (BBuiltin (TyArr _ (TyB _ TyFloat) _) Max) e) e') =
-        case (e, e') of
-            (FloatLit _ x, FloatLit _ y) -> FloatLit tyF (max x y)
-            _                            -> embed e0
-    a e0@(EAppF _ (EApp _ (BBuiltin (TyArr _ (TyB _ TyFloat) _) Min) e) e') =
-        case (e, e') of
-            (FloatLit _ x, FloatLit _ y) -> FloatLit tyF (min x y)
-            _                            -> embed e0
-    a e0@(EAppF _ (EApp _ (BBuiltin (TyArr _ (TyB _ TyInteger) _) Minus) e) e') =
-        case (e, e') of
-            (IntLit _ i, IntLit _ j) -> IntLit tyI (i-j)
-            _                        -> embed e0
-    a e0@(EAppF _ (EApp _ (BBuiltin (TyArr _ (TyB _ TyInteger) _) Times) e) e') =
-        case (e, e') of
-            (IntLit _ i, IntLit _ j) -> IntLit tyI (i*j)
-            _                        -> embed e0
-    a e0@(EAppF _ (EApp _ (BBuiltin (TyArr _ (TyB _ TyFloat) _) Plus) e) e') =
-        case (e, e') of
-            (FloatLit _ i, FloatLit _ j) -> FloatLit tyF (i+j)
-            _                            -> embed e0
-    a e0@(EAppF _ (EApp _ (BBuiltin (TyArr _ (TyB _ TyFloat) _) Minus) e) e') =
-        case (e, e') of
-            (FloatLit _ i, FloatLit _ j) -> FloatLit tyF (i-j)
-            _                            -> embed e0
-    a e0@(EAppF _ (EApp _ (BBuiltin (TyArr _ (TyB _ TyFloat) _) Times) e) e') =
-        case (e, e') of
-            (FloatLit _ i, FloatLit _ j) -> FloatLit tyF (i*j)
-            _                            -> embed e0
-    a e0@(EAppF _ (EApp _ (BBuiltin (TyArr _ (TyB _ TyFloat) _) Div) e) e') =
-        case (e, e') of
-            (FloatLit _ i, FloatLit _ j) -> FloatLit tyF (i/j)
-            _                            -> embed e0
-    a e0@(EAppF _ (UBuiltin _ Tally) e) =
-        case e of
-            StrLit _ str -> IntLit tyI (fromIntegral $ BS.length str)
-            _            -> embed e0
-    a e0@(EAppF _ (EApp _ (BBuiltin (TyArr _ (TyB _ TyInteger) _) Lt) e) e') =
-        case (e, e') of
-            (IntLit _ i, IntLit _ j) -> BoolLit tyBool (i < j)
-            _                        -> embed e0
-    a e0@(EAppF _ (EApp _ (BBuiltin (TyArr _ (TyB _ TyInteger) _) Gt) e) e') =
-        case (e, e') of
-            (IntLit _ i, IntLit _ j) -> BoolLit tyBool (i > j)
-            _                        -> embed e0
-    a e0@(EAppF _ (EApp _ (BBuiltin (TyArr _ (TyB _ TyInteger) _) Eq) e) e') =
-        case (e, e') of
-            (IntLit _ i, IntLit _ j) -> BoolLit tyBool (i == j)
-            _                        -> embed e0
-    a e0@(EAppF _ (EApp _ (BBuiltin (TyArr _ (TyB _ TyStr) _) Eq) e) e') =
-        case (e, e') of
-            (StrLit _ i, StrLit _ j) -> BoolLit tyBool (i == j)
-            _                        -> embed e0
-    a e0@(EAppF _ (EApp _ (BBuiltin (TyArr _ (TyB _ TyInteger) _) Neq) e) e') =
-        case (e, e') of
-            (IntLit _ i, IntLit _ j) -> BoolLit tyBool (i /= j)
-            _                        -> embed e0
-    a e0@(EAppF _ (EApp _ (BBuiltin (TyArr _ (TyB _ TyStr) _) Neq) e) e') =
-        case (e, e') of
-            (StrLit _ i, StrLit _ j) -> BoolLit tyBool (i /= j)
-            _                        -> embed e0
-    a e0@(EAppF _ (EApp _ (BBuiltin _ And) e) e') =
-        case (e, e') of
-            (BoolLit _ b, BoolLit _ b') -> BoolLit tyBool (b && b')
-            _                           -> embed e0
-    a e0@(EAppF _ (EApp _ (BBuiltin _ Or) e) e') =
-        case (e, e') of
-            (BoolLit _ b, BoolLit _ b') -> BoolLit tyBool (b || b')
-            _                           -> embed e0
-    a (EAppF _ (EApp _ (UBuiltin _ Const) e) _) = e
-    a e@(EAppF _ (UBuiltin _ Const) _) = embed e
+eNorm e@Field{}       = e
+eNorm e@IParseField{} = e
+eNorm e@FParseField{} = e
+eNorm e@IntLit{}      = e
+eNorm e@FloatLit{}    = e
+eNorm e@BoolLit{}     = e
+eNorm e@StrLit{}      = e
+eNorm e@RegexLit{}    = e
+eNorm e@RegexCompiled{} = e
+eNorm e@UBuiltin{}    = e
+eNorm e@Column{}      = e
+eNorm e@AllColumn{}   = e
+eNorm e@IParseCol{}   = e
+eNorm e@FParseCol{}   = e
+eNorm e@AllField{}    = e
+eNorm e@Guarded{}     = e
+eNorm e@Lam{}         = e
+eNorm e@BBuiltin{}    = e
+eNorm e@TBuiltin{}    = e
+eNorm e@Tup{}         = e
+eNorm e@Ix{}          = e
+eNorm e@(EApp _ BBuiltin{} _) = e
+eNorm e0@(EApp _ (EApp _ (BBuiltin _ Matches) e) e') =
+    let eI = eNorm e
+        eI' = eNorm e'
+    in case (eI, eI') of
+        (RegexCompiled re, StrLit _ str) -> BoolLit tyBool (isMatch' re str)
+        (StrLit _ str, RegexCompiled re) -> BoolLit tyBool (isMatch' re str)
+        _                                -> e0
+eNorm e0@(EApp _ (EApp _ (BBuiltin _ NotMatches) e) e') =
+    let eI = eNorm e
+        eI' = eNorm e'
+    in case (eI, eI') of
+        (RegexCompiled re, StrLit _ str) -> BoolLit tyBool (not $ isMatch' re str)
+        (StrLit _ str, RegexCompiled re) -> BoolLit tyBool (not $ isMatch' re str)
+        _                                -> e0
+eNorm e0@(EApp _ (EApp _ (BBuiltin (TyArr _ (TyB _ TyInteger) _) Plus) e) e') =
+    let eI = eNorm e
+        eI' = eNorm e'
+    in case (eI, eI') of
+        (IntLit _ i, IntLit _ j) -> IntLit tyI (i+j)
+        _                        -> e0
+eNorm e0@(EApp _ (EApp _ (BBuiltin (TyArr _ (TyB _ TyInteger) _) Max) e) e') =
+    let eI = eNorm e
+        eI' = eNorm e'
+    in case (eI, eI') of
+        (IntLit _ i, IntLit _ j) -> IntLit tyI (max i j)
+        _                        -> e0
+eNorm e0@(EApp _ (EApp _ (BBuiltin (TyArr _ (TyB _ TyInteger) _) Min) e) e') =
+    let eI = eNorm e
+        eI' = eNorm e'
+    in case (eI, eI') of
+        (IntLit _ i, IntLit _ j) -> IntLit tyI (min i j)
+        _                        -> e0
+eNorm e0@(EApp _ (EApp _ (BBuiltin (TyArr _ (TyB _ TyFloat) _) Max) e) e') =
+    let eI = eNorm e
+        eI' = eNorm e'
+    in case (eI, eI') of
+        (FloatLit _ x, FloatLit _ y) -> FloatLit tyF (max x y)
+        _                            -> e0
+eNorm e0@(EApp _ (EApp _ (BBuiltin (TyArr _ (TyB _ TyFloat) _) Min) e) e') =
+    let eI = eNorm e
+        eI' = eNorm e'
+    in case (eI, eI') of
+        (FloatLit _ x, FloatLit _ y) -> FloatLit tyF (min x y)
+        _                            -> e0
+eNorm e0@(EApp _ (EApp _ (BBuiltin (TyArr _ (TyB _ TyInteger) _) Minus) e) e') =
+    let eI = eNorm e
+        eI' = eNorm e'
+    in case (eI, eI') of
+        (IntLit _ i, IntLit _ j) -> IntLit tyI (i-j)
+        _                        -> e0
+eNorm e0@(EApp _ (EApp _ (BBuiltin (TyArr _ (TyB _ TyInteger) _) Times) e) e') =
+    let eI = eNorm e
+        eI' = eNorm e'
+    in case (eI, eI') of
+        (IntLit _ i, IntLit _ j) -> IntLit tyI (i*j)
+        _                        -> e0
+eNorm e0@(EApp _ (EApp _ (BBuiltin (TyArr _ (TyB _ TyFloat) _) Plus) e) e') =
+    let eI = eNorm e
+        eI' = eNorm e'
+    in case (eI, eI') of
+        (FloatLit _ i, FloatLit _ j) -> FloatLit tyF (i+j)
+        _                            -> e0
+eNorm e0@(EApp _ (EApp _ (BBuiltin (TyArr _ (TyB _ TyFloat) _) Minus) e) e') =
+    let eI = eNorm e
+        eI' = eNorm e'
+    in case (eI, eI') of
+        (FloatLit _ i, FloatLit _ j) -> FloatLit tyF (i-j)
+        _                            -> e0
+eNorm e0@(EApp _ (EApp _ (BBuiltin (TyArr _ (TyB _ TyFloat) _) Times) e) e') =
+    let eI = eNorm e
+        eI' = eNorm e'
+    in case (eI, eI') of
+        (FloatLit _ i, FloatLit _ j) -> FloatLit tyF (i*j)
+        _                            -> e0
+eNorm e0@(EApp _ (EApp _ (BBuiltin (TyArr _ (TyB _ TyFloat) _) Div) e) e') =
+    let eI = eNorm e
+        eI' = eNorm e'
+    in case (eI, eI') of
+        (FloatLit _ i, FloatLit _ j) -> FloatLit tyF (i/j)
+        _                            -> e0
+eNorm e0@(EApp _ (UBuiltin _ Tally) e) =
+    let eI = eNorm e
+    in case eI of
+        StrLit _ str -> IntLit tyI (fromIntegral $ BS.length str)
+        _            -> e0
+eNorm e0@(EApp _ (EApp _ (BBuiltin (TyArr _ (TyB _ TyInteger) _) Lt) e) e') =
+    let eI = eNorm e
+        eI' = eNorm e'
+    in case (eI, eI') of
+        (IntLit _ i, IntLit _ j) -> BoolLit tyBool (i < j)
+        _                        -> e0
+eNorm e0@(EApp _ (EApp _ (BBuiltin (TyArr _ (TyB _ TyInteger) _) Gt) e) e') =
+    let eI = eNorm e
+        eI' = eNorm e'
+    in case (eI, eI') of
+        (IntLit _ i, IntLit _ j) -> BoolLit tyBool (i > j)
+        _                        -> e0
+eNorm e0@(EApp _ (EApp _ (BBuiltin (TyArr _ (TyB _ TyInteger) _) Eq) e) e') =
+    let eI = eNorm e
+        eI' = eNorm e'
+    in case (eI, eI') of
+        (IntLit _ i, IntLit _ j) -> BoolLit tyBool (i == j)
+        _                        -> e0
+eNorm e0@(EApp _ (EApp _ (BBuiltin (TyArr _ (TyB _ TyStr) _) Eq) e) e') =
+    let eI = eNorm e
+        eI' = eNorm e'
+    in case (eI, eI') of
+        (StrLit _ i, StrLit _ j) -> BoolLit tyBool (i == j)
+        _                        -> e0
+eNorm e0@(EApp _ (EApp _ (BBuiltin (TyArr _ (TyB _ TyInteger) _) Neq) e) e') =
+    let eI = eNorm e
+        eI' = eNorm e'
+    in case (eI, eI') of
+        (IntLit _ i, IntLit _ j) -> BoolLit tyBool (i /= j)
+        _                        -> e0
+eNorm e0@(EApp _ (EApp _ (BBuiltin (TyArr _ (TyB _ TyStr) _) Neq) e) e') =
+    let eI = eNorm e
+        eI' = eNorm e'
+    in case (eI, eI') of
+        (StrLit _ i, StrLit _ j) -> BoolLit tyBool (i /= j)
+        _                        -> e0
+eNorm e0@(EApp _ (EApp _ (BBuiltin _ And) e) e') =
+    let eI = eNorm e
+        eI' = eNorm e'
+    in case (eI, eI') of
+        (BoolLit _ b, BoolLit _ b') -> BoolLit tyBool (b && b')
+        _                           -> e0
+eNorm e0@(EApp _ (EApp _ (BBuiltin _ Or) e) e') =
+    let eI = eNorm e
+        eI' = eNorm e'
+    in case (eI, eI') of
+        (BoolLit _ b, BoolLit _ b') -> BoolLit tyBool (b || b')
+        _                           -> e0
+eNorm (EApp _ (EApp _ (UBuiltin _ Const) e) _) = e
+eNorm e@(EApp _ (UBuiltin _ Const) _) = e
