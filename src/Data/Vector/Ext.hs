fb2c65294ca1f89810fe9af7c1cfdc6a3948aeaf
Vanessa McHale <vamchale@gmail.com>
2023-03-11 17:04:12 -0500

    rip out old?

--- src/Data/Vector/Ext.hs
+++
@@ -1,7 +0,0 @@
-module Data.Vector.Ext ( priorM_
-                       ) where
-
-import qualified Data.Vector as V
-
-priorM_ :: Monad m => (a -> a -> m b) -> V.Vector a -> m ()
-priorM_ op xs = V.zipWithM_ op (V.tail xs) xs
