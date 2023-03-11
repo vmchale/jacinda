dcaae85191c3303e51031429491d6e015d3cb456
Vanessa McHale <vamchale@gmail.com>
2023-03-11 17:04:12 -0500

    rip out old?

--- src/Jacinda/Ty.hs
+++ src/Jacinda/Ty.hs
@@ -10,20 +10,17 @@ module Jacinda.Ty ( TyM
                   ) where
 
 import           Control.Exception          (Exception)
-import           Control.Monad              (forM)
 import           Control.Monad.Except       (liftEither, throwError)
 import           Control.Monad.State.Strict (StateT, gets, modify, runState, runStateT)
 import           Data.Bifunctor             (first, second)
 import           Data.Foldable              (traverse_)
 import           Data.Functor               (void, ($>))
 import qualified Data.IntMap                as IM
-import           Data.Maybe                 (fromMaybe)
 import           Data.Semigroup             ((<>))
 import qualified Data.Set                   as S
 import qualified Data.Text                  as T
 import           Data.Typeable              (Typeable)
 import qualified Data.Vector                as V
-import qualified Data.Vector.Ext            as V
 import           Intern.Name
 import           Intern.Unique
 import           Jacinda.AST
@@ -52,43 +49,32 @@ instance (Typeable a, Pretty a) => Exception (Err a) where
 
 -- solve, unify etc. THEN check that all constraints are satisfied?
 -- (after accumulating classVar membership...)
-data TyState a = TyState { maxU        :: !Int
-                         , kindEnv     :: IM.IntMap K
-                         , classVars   :: IM.IntMap (S.Set (C, a))
-                         , varEnv      :: IM.IntMap (T K)
-                         , constraints :: S.Set (a, T K, T K)
+data TyState a = TyState { maxU      :: !Int
+                         , kindEnv   :: IM.IntMap K
+                         , classVars :: IM.IntMap (S.Set (C, a))
+                         , varEnv    :: IM.IntMap (T K)
                          }
 
-prettyConstraints :: S.Set (b, T a, T a) -> Doc ann
-prettyConstraints cs = vsep (prettyEq . go <$> S.toList cs) where
-    go (_, x, y) = (x, y)
-
-prettyEq :: (T a, T a) -> Doc ann
-prettyEq (ty, ty') = pretty ty <+> "≡" <+> pretty ty'
-
 mapMaxU :: (Int -> Int) -> TyState a -> TyState a
-mapMaxU f (TyState u k c v cs) = TyState (f u) k c v cs
+mapMaxU f (TyState u k c v) = TyState (f u) k c v
 
 setMaxU :: Int -> TyState a -> TyState a
 -- setMaxU i = mapMaxU (const i)
-setMaxU i (TyState _ k c v cs) = TyState i k c v cs
+setMaxU i (TyState _ k c v) = TyState i k c v
 
 mapCV :: (IM.IntMap (S.Set (C, a)) -> IM.IntMap (S.Set (C, a))) -> TyState a -> TyState a
-mapCV f (TyState u k cvs v cs) = TyState u k (f cvs) v cs
+mapCV f (TyState u k cvs v) = TyState u k (f cvs) v
 
 addVarEnv :: Int -> T K -> TyState a -> TyState a
-addVarEnv i ty (TyState u k cvs v cs) = TyState u k cvs (IM.insert i ty v) cs
+addVarEnv i ty (TyState u k cvs v) = TyState u k cvs (IM.insert i ty v)
 
 addKindEnv :: Int -> K -> TyState a -> TyState a
-addKindEnv i k (TyState u ks cvs v cs) = TyState u (IM.insert i k ks) cvs v cs
-
-addConstraint :: Ord a => (a, T K, T K) -> TyState a -> TyState a
-addConstraint c (TyState u k cvs v cs) = TyState u k cvs v (S.insert c cs)
+addKindEnv i k (TyState u ks cvs v) = TyState u (IM.insert i k ks) cvs v
 
 type TyM a = StateT (TyState a) (Either (Err a))
 
 runTyM :: Int -> TyM a b -> Either (Err a) (b, Int)
-runTyM i = fmap (second maxU) . flip runStateT (TyState i IM.empty IM.empty IM.empty S.empty)
+runTyM i = fmap (second maxU) . flip runStateT (TyState i IM.empty IM.empty IM.empty)
 
 type Subst a = IM.IntMap (T a)
 
@@ -109,16 +95,6 @@ aT _ ty'@TyB{} = ty'
 aT um (TyApp l ty ty') = TyApp l (aT um ty) (aT um ty')
 aT um (TyArr l ty ty') = TyArr l (aT um ty) (aT um ty')
 aT um (TyTup l tys)    = TyTup l (aT um <$> tys)
-
--- | Perform substitutions before handing off to 'unifyMatch'
-unifyPrep :: Subst a
-          -> [(l, T a, T a)]
-          -> TyM l (IM.IntMap (T a))
-unifyPrep _ [] = pure mempty
-unifyPrep um ((l, ty, ty'):tys) =
-    let ty'' = aT um ty
-        ty''' = aT um ty'
-    in unifyMatch um $ (l, ty'', ty'''):tys
 
 mguPrep :: l -> Subst a -> T a -> T a -> Either (Err l) (Subst a)
 mguPrep l s t0 t1 =
@@ -145,25 +121,6 @@ tS_ f s (t:ts) = do{next <- f s t; tS_ f next ts}
 zS _ s [] _           = pure s
 zS _ s _ []           = pure s
 zS op s (x:xs) (y:ys) = do{next <- op s x y; zS op next xs ys}
-
-unifyMatch :: Subst a -> [(l, T a, T a)] -> TyM l (IM.IntMap (T a))
-unifyMatch _ [] = pure IM.empty
-unifyMatch um ((_, TyB _ b, TyB _ b'):tys) | b == b' = unifyPrep um tys
-unifyMatch um ((_, TyVar _ n, ty@(TyVar _ n')):tys)
-    | n == n' = unifyPrep um tys
-unifyMatch um ((_, ty, TyVar  _ (Name _ (Unique k) _)):tys) = IM.insert k ty <$> unifyPrep (IM.insert k ty um) tys
-unifyMatch um ((_, TyVar _ (Name _ (Unique k) _), ty):tys) = IM.insert k ty <$> unifyPrep (IM.insert k ty um) tys
-unifyMatch um ((l, TyApp _ ty ty', TyApp _ ty'' ty'''):tys) = unifyPrep um ((l, ty, ty'') : (l, ty', ty''') : tys)
-unifyMatch um ((l, TyArr _ ty ty', TyArr _ ty'' ty'''):tys) = unifyPrep um ((l, ty, ty'') : (l, ty', ty''') : tys)
-unifyMatch um ((l, TyTup _ tys, TyTup _ tys'):tyss)
-    | length tys == length tys' = unifyPrep um (zip3 (repeat l) tys tys' ++ tyss)
-unifyMatch _ ((l, ty, ty'):_) = throwError (UF l (void ty) (void ty'))
-
-unify :: [(l, T a, T a)] -> TyM l (IM.IntMap (T a))
-unify = unifyPrep IM.empty
-
-unifyM :: S.Set (l, T a, T a) -> TyM l (IM.IntMap (T a))
-unifyM s = {-# SCC "unifyM" #-} unify (S.toList s)
 
 substInt :: IM.IntMap (T a) -> Int -> Maybe (T a)
 substInt tys k =
@@ -203,11 +160,6 @@ tyArr = TyArr Star
 
 var :: Name K -> T K
 var = TyVar Star
-
--- assumes they have been renamed...
-pushConstraint :: Ord a => a -> T K -> T K -> TyM a ()
-pushConstraint l ty ty' =
-    modify (addConstraint (l, ty, ty'))
 
 isStar :: K -> TyM a ()
 isStar Star = pure ()
@@ -272,7 +224,7 @@ kind (TyApp k1 ty0 ty1) = do
         k0                               -> throwError $ Expected (KArr Star Star) k0
 
 checkType :: Ord a => T K -> (C, a) -> TyM a ()
-checkType TyVar{} _                            = pure () -- TODO: I think this is right
+checkType TyVar{} _                            = pure ()
 checkType (TyB _ TyR) (IsSemigroup, _)         = pure ()
 checkType (TyB _ TyStr) (IsSemigroup, _)       = pure ()
 checkType (TyB _ TyInteger) (IsSemigroup, _)   = pure ()
@@ -478,7 +430,7 @@ tyES s (UBuiltin l Dedup) = do {a <- dummyName "a"; modify (mapCV (addC a (IsEq,
 tyES s (UBuiltin _ Const) = do {a <- var <$> dummyName "a"; b <- var <$> dummyName "b"; pure (UBuiltin (tyArr a (tyArr b a)) Const, s)}
 tyES s (UBuiltin l CatMaybes) = do {a <- dummyName "a"; f <- higherOrder "f"; modify (mapCV (addC f (Witherable, l))); let a'=var a; f'=var f in pure (UBuiltin (tyArr (hkt f' (tyOpt a')) (hkt f' a')) CatMaybes, s)}
 tyES s (BBuiltin l Filter) = do {a <- dummyName "a"; f <- higherOrder "f"; modify (mapCV (addC f (Witherable, l))); let a'=var a; f'=var f; w=hkt f' a' in pure (BBuiltin (tyArr (tyArr a' tyB) (tyArr w w)) Filter, s)}
-tyES s (UBuiltin l (Select i)) = do
+tyES s (UBuiltin _ (Select i)) = do
     ρ <- dummyName "ρ"; a <- var <$> dummyName "a"
     pure (UBuiltin (tyArr (Rho Star ρ (IM.singleton i a)) a) (Select i), s)
 tyES s (BBuiltin l MapMaybe) = do
@@ -551,6 +503,11 @@ tyES s (OptionVal _ Nothing) = do {a <- var <$> dummyName "a"; pure (OptionVal (
 tyES s (Arr l v) | V.null v = do
     a <- var <$> dummyName "a"
     pure (Arr (tyV a) V.empty, s)
+                 | otherwise = do
+    (v',s0) <- tS tyES s (V.toList v)
+    let vt=fmap eLoc v'
+    s1 <- liftEither $ zS (mguPrep l) s0 vt (tail vt)
+    pure (Arr (head vt) (V.fromList v'), s1)
 tyES s (Cond l p e0 e1) = do
     (p', s0) <- tyES s p
     (e0', s1) <- tyES s0 e0
@@ -559,267 +516,10 @@ tyES s (Cond l p e0 e1) = do
     s3 <- liftEither $ mguPrep l s2 tyB (eLoc p')
     s4 <- liftEither $ mguPrep l s3 t (eLoc e1')
     pure (Cond t p' e0' e1', s4)
-
-tyE0 :: Ord a => E a -> TyM a (E (T K))
-tyE0 (BoolLit _ b)           = pure $ BoolLit tyB b
-tyE0 (IntLit _ i)            = pure $ IntLit tyI i
-tyE0 (FloatLit _ f)          = pure $ FloatLit tyF f
-tyE0 (StrLit _ str)          = pure $ StrLit tyStr str
-tyE0 (RegexLit _ rr)         = pure $ RegexLit tyR rr
-tyE0 (Column _ i)            = pure $ Column (tyStream tyStr) i
-tyE0 (IParseCol _ i)         = pure $ IParseCol (tyStream tyI) i
-tyE0 (FParseCol _ i)         = pure $ FParseCol (tyStream tyF) i
-tyE0 (Field _ i)             = pure $ Field tyStr i
-tyE0 (LastField _)           = pure $ LastField tyStr
-tyE0 AllField{}              = pure $ AllField tyStr
-tyE0 AllColumn{}             = pure $ AllColumn (tyStream tyStr)
-tyE0 (NBuiltin _ Ix)         = pure $ NBuiltin tyI Ix
-tyE0 (NBuiltin _ Fp)         = pure $ NBuiltin tyStr Fp
-tyE0 (NBuiltin _ Nf)         = pure $ NBuiltin tyI Nf
-tyE0 (BBuiltin l Plus)       = BBuiltin <$> tySemiOp l <*> pure Plus
-tyE0 (BBuiltin l Minus)      = BBuiltin <$> tyNumOp l <*> pure Minus
-tyE0 (BBuiltin l Times)      = BBuiltin <$> tyNumOp l <*> pure Times
-tyE0 (BBuiltin l Exp)        = BBuiltin <$> tyNumOp l <*> pure Exp
-tyE0 (BBuiltin l Gt)         = BBuiltin <$> tyOrd l <*> pure Gt
-tyE0 (BBuiltin l Lt)         = BBuiltin <$> tyOrd l <*> pure Lt
-tyE0 (BBuiltin l Geq)        = BBuiltin <$> tyOrd l <*> pure Geq
-tyE0 (BBuiltin l Leq)        = BBuiltin <$> tyOrd l <*> pure Leq
-tyE0 (BBuiltin l Eq)         = BBuiltin <$> tyEq l <*> pure Eq
-tyE0 (BBuiltin l Neq)        = BBuiltin <$> tyEq l <*> pure Neq
-tyE0 (BBuiltin l Min)        = BBuiltin <$> tyM l <*> pure Min
-tyE0 (BBuiltin l Max)        = BBuiltin <$> tyM l <*> pure Max
-tyE0 (BBuiltin _ Split)      = pure $ BBuiltin (tyArr tyStr (tyArr tyR (tyV tyStr))) Split
-tyE0 (BBuiltin _ Splitc)     = pure $ BBuiltin (tyArr tyStr (tyArr tyStr (tyV tyStr))) Splitc
-tyE0 (BBuiltin _ Matches)    = pure $ BBuiltin (tyArr tyStr (tyArr tyR tyB)) Matches
-tyE0 (BBuiltin _ NotMatches) = pure $ BBuiltin (tyArr tyStr (tyArr tyR tyB)) NotMatches
-tyE0 (UBuiltin _ Tally)      = pure $ UBuiltin (tyArr tyStr tyI) Tally
-tyE0 (BBuiltin _ Div)        = pure $ BBuiltin (tyArr tyF (tyArr tyF tyF)) Div
-tyE0 (UBuiltin _ Not)        = pure $ UBuiltin (tyArr tyB tyB) Not
-tyE0 (BBuiltin _ And)        = pure $ BBuiltin (tyArr tyB (tyArr tyB tyB)) And
-tyE0 (BBuiltin _ Or)         = pure $ BBuiltin (tyArr tyB (tyArr tyB tyB)) Or
-tyE0 (BBuiltin _ Match)      = pure $ BBuiltin (tyArr tyStr (tyArr tyR (tyOpt $ TyTup Star [tyI, tyI]))) Match
-tyE0 (TBuiltin _ Substr)     = pure $ TBuiltin (tyArr tyStr (tyArr tyI (tyArr tyI tyStr))) Substr
-tyE0 (UBuiltin _ IParse)     = pure $ UBuiltin (tyArr tyStr tyI) IParse
-tyE0 (UBuiltin _ FParse)     = pure $ UBuiltin (tyArr tyStr tyF) FParse
-tyE0 (UBuiltin _ Floor)      = pure $ UBuiltin (tyArr tyF tyI) Floor
-tyE0 (UBuiltin _ Ceiling)    = pure $ UBuiltin (tyArr tyF tyI) Ceiling
-tyE0 (UBuiltin _ TallyList) = do
-    a <- dummyName "a"
-    let a' = var a
-    pure $ UBuiltin (tyArr a' tyI) TallyList
-tyE0 (UBuiltin l Negate) = do
-    a <- dummyName "a"
-    modify (mapCV (addC a (IsNum, l)))
-    let a' = var a
-    pure $ UBuiltin (tyArr a' a') Negate
-tyE0 (UBuiltin _ Some) = do
-    a <- dummyName "a"
-    let a' = var a
-    pure $ UBuiltin (tyArr a' (tyOpt a')) Some
-tyE0 (NBuiltin _ None) = do
-    a <- dummyName "a"
-    pure $ NBuiltin (tyOpt $ var a) None
-tyE0 (ParseCol l i) = do
-    a <- dummyName "a"
-    let a' = var a
-    modify (mapCV (addC a (IsParse, l)))
-    pure $ ParseCol (tyStream a') i
-tyE0 (UBuiltin l Parse) = do
-    a <- dummyName "a"
-    let a' = var a
-    modify (mapCV (addC a (IsParse, l)))
-    pure $ UBuiltin (tyArr tyStr a') Parse
-tyE0 (BBuiltin l Sprintf) = do
-    a <- dummyName "a"
-    let a' = var a
-    modify (mapCV (addC a (IsPrintf, l)))
-    pure $ BBuiltin (tyArr tyStr (tyArr a' tyStr)) Sprintf
-tyE0 (BBuiltin l DedupOn) = do
-    a <- dummyName "a"
-    b <- dummyName "b"
-    let a' = var a
-        b' = var b
-    modify (mapCV (addC b (IsEq, l)))
-    pure $ BBuiltin (tyArr (tyArr a' b') (tyArr (tyStream a') (tyStream a'))) DedupOn
-tyE0 (UBuiltin _ (At i)) = do
-    a <- dummyName "a"
-    let a' = var a
-        tyVϵ = tyV a'
-    pure $ UBuiltin (tyArr tyVϵ a') (At i)
-tyE0 (UBuiltin l Dedup) = do
-    a <- dummyName "a"
-    let a' = var a
-        fTy = tyArr (tyStream a') (tyStream a')
-    modify (mapCV (addC a (IsEq, l)))
-    pure $ UBuiltin fTy Dedup
-tyE0 (UBuiltin _ Const) = do
-    a <- dummyName "a"
-    b <- dummyName "b"
-    let a' = var a
-        b' = var b
-        fTy = tyArr a' (tyArr b' a')
-    pure $ UBuiltin fTy Const
-tyE0 (UBuiltin l CatMaybes) = do
-    a <- dummyName "a"
-    f <- higherOrder "f"
-    let a' = var a
-        f' = var f
-        fTy = tyArr (hkt f' $ tyOpt a') (hkt f' a')
-    modify (mapCV (addC f (Witherable, l)))
-    pure $ UBuiltin fTy CatMaybes
-tyE0 (BBuiltin l Filter) = do
-    a <- dummyName "a"
-    f <- higherOrder "f"
-    let a' = var a
-        f' = var f
-        fTy = tyArr (tyArr a' tyB) (tyArr (hkt f' a') (hkt f' a'))
-    modify (mapCV (addC f (Witherable , l)))
-    pure $ BBuiltin fTy Filter
-tyE0 (BBuiltin l MapMaybe) = do
-    a <- dummyName "a"
-    b <- dummyName "b"
-    f <- higherOrder "f"
-    let a' = var a
-        b' = var b
-        f' = var f
-        fTy = tyArr (tyArr a' (tyOpt b')) (tyArr (hkt f' a') (hkt f' b'))
-    modify (mapCV (addC f (Witherable, l)))
-    pure $ BBuiltin fTy MapMaybe
-tyE0 (BBuiltin l Map) = do
-    a <- dummyName "a"
-    b <- dummyName "b"
-    f <- higherOrder "f"
-    let a' = var a
-        b' = var b
-        f' = var f
-        fTy = tyArr (tyArr a' b') (tyArr (hkt f' a') (hkt f' b'))
-    modify (mapCV (addC f (Functor, l)))
-    pure $ BBuiltin fTy Map
-tyE0 (TBuiltin l Fold) = do
-    b <- dummyName "b"
-    a <- dummyName "a"
-    f <- higherOrder "f"
-    let b' = var b
-        a' = var a
-        f' = var f
-        fTy = tyArr (tyArr b' (tyArr a' b')) (tyArr b' (tyArr (hkt f' a') b'))
-    modify (mapCV (addC f (Foldable, l)))
-    pure $ TBuiltin fTy Fold
-tyE0 (BBuiltin l Fold1) = do
-    a <- dummyName "a"
-    f <- higherOrder "f"
-    let a' = var a
-        f' = var f
-        fTy = tyArr (tyArr a' (tyArr a' a')) (tyArr (hkt f' a') a')
-    modify (mapCV (addC f (Foldable, l)))
-    pure $ BBuiltin fTy Fold1
-tyE0 (TBuiltin _ Captures) =
-    pure $ TBuiltin (tyArr tyStr (tyArr tyI (tyArr tyR (tyOpt tyStr)))) Captures
--- (a -> a -> a) -> Stream a -> Stream a
-tyE0 (BBuiltin _ Prior) = do
-    a <- dummyName "a"
-    b <- dummyName "b"
-    let a' = var a
-        b' = var b
-        fTy = tyArr (tyArr a' (tyArr a' b')) (tyArr (tyStream a') (tyStream b'))
-    pure $ BBuiltin fTy Prior
--- (a -> b -> c) -> Stream a -> Stream b -> Stream c
-tyE0 (TBuiltin _ ZipW) = do
-    a <- dummyName "a"
-    b <- dummyName "b"
-    c <- dummyName "c"
-    let a' = var a
-        b' = var b
-        c' = var c
-        fTy = tyArr (tyArr a' (tyArr b' c')) (tyArr (tyStream a') (tyArr (tyStream b') (tyStream c')))
-    pure $ TBuiltin fTy ZipW
--- (b -> a -> b) -> b -> Stream a -> Stream b
-tyE0 (TBuiltin _ Scan) = do
-    b <- dummyName "b"
-    a <- dummyName "a"
-    let b' = var b
-        a' = var a
-        fTy = tyArr (tyArr b' (tyArr a' b')) (tyArr b' (tyArr (tyStream a') (tyStream b')))
-    pure $ TBuiltin fTy Scan
-tyE0 (TBuiltin _ Option) = do
-    b <- dummyName "b"
-    a <- dummyName "a"
-    let b' = var b
-        a' = var a
-        fTy = tyArr b' (tyArr (tyArr a' b') (tyArr (tyOpt a') b'))
-    pure $ TBuiltin fTy Option
-tyE0 (TBuiltin _ AllCaptures) =
-    pure $ TBuiltin (tyArr tyStr (tyArr tyI (tyArr tyR (tyV tyStr)))) AllCaptures
-tyE0 (Implicit _ e) = do
-    e' <- tyE0 e
-    pure $ Implicit (tyStream (eLoc e')) e'
-tyE0 (Guarded l e streamE) = do
-    streamE' <- tyE0 streamE
-    e' <- tyE0 e
-    pushConstraint l tyB (eLoc e')
-    pure $ Guarded (tyStream (eLoc streamE')) e' streamE'
-tyE0 (EApp _ e0 e1) = do
-    e0' <- tyE0 e0
-    e1' <- tyE0 e1
-    a <- dummyName "a"
-    b <- dummyName "b"
-    let a' = var a
-        b' = var b
-        fTy = tyArr a' b'
-    pushConstraint (eLoc e0) fTy (eLoc e0')
-    pushConstraint (eLoc e1) a' (eLoc e1')
-    pure $ EApp b' e0' e1'
-tyE0 (Lam _ n@(Name _ (Unique i) _) e) = do
-    a <- dummyName "a"
-    let a' = var a
-    modify (addVarEnv i a')
-    e' <- tyE0 e
-    pure $ Lam (tyArr a' (eLoc e')) (n $> a') e'
-tyE0 (Let _ (n@(Name _ (Unique i) _), eϵ) e) = do
-    eϵ' <- tyE0 eϵ
-    let bTy = eLoc eϵ'
-    modify (addVarEnv i bTy)
-    e' <- tyE0 e
-    pure $ Let (eLoc e') (n $> bTy, eϵ') e'
-tyE0 (Tup _ es) = do
-    es' <- traverse tyE0 es
-    pure $ Tup (TyTup Star (eLoc <$> es')) es'
-tyE0 (Var _ n) = do
-    ty <- lookupVar n
-    pure (Var ty (n $> ty))
-tyE0 Dfn{} = desugar
-tyE0 (ResVar _ X) = desugar
-tyE0 (ResVar _ Y) = desugar
-tyE0 RegexCompiled{} = error "Regex should not be compiled at this stage."
-tyE0 Paren{} = desugar
-tyE0 (OptionVal _ (Just e)) = do
-    e' <- tyE0 e
-    pure $ OptionVal (tyOpt $ eLoc e') (Just e')
-tyE0 (OptionVal _ Nothing) = do
-    a <- dummyName "a"
-    let a' = var a
-    pure $ OptionVal (tyOpt a') Nothing
-tyE0 (Arr l v) | V.null v = do
-    a <- dummyName "a"
-    let a' = var a
-    pure $ Arr (tyV a') V.empty
-               | otherwise = do
-    v' <- traverse tyE0 v
-    let x = V.head v'
-    V.priorM_ (\y y' -> pushConstraint l (eLoc y) (eLoc y')) v'
-    pure $ Arr (eLoc x) v'
-tyE0 (Anchor l es) = do
-    es' <- forM es $ \e -> do
-        e' <- tyE0 e
-        a <- dummyName "a"
-        let a' = var a
-        pushConstraint l (tyStream a') (eLoc e') $> e'
-    pure $ Anchor (TyB Star TyUnit) es'
-tyE0 (Cond l p e0 e1) = do
-    p' <- tyE0 p
-    e0' <- tyE0 e0
-    e1' <- tyE0 e1
-    let ty0 = eLoc e0'
-    pushConstraint l tyB (eLoc p')
-    pushConstraint (eLoc e0) ty0 (eLoc e1')
-    pure $ Cond ty0 p' e0' e1'
+tyES _ RegexCompiled{} = error "Regex should not be compiled at this stage."
+tyES _ Dfn{} = desugar
+tyES _ ResVar{} = desugar
+tyES _ Paren{} = desugar
+tyES s (Anchor l es) = do
+    (es', s') <- tS (\sϵ e -> do {(e',s0) <- tyES sϵ e; a <- var <$> dummyName "a"; s1 <- liftEither $ mguPrep l s0 (tyStream a) (eLoc e'); pure (e', s1)}) s es
+    pure (Anchor (TyB Star TyUnit) es', s')
