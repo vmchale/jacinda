82b8d90cf9d7fb52a8c6a41f2bc631fa9158b198
Vanessa McHale <vamchale@gmail.com>
2023-04-04 17:58:24 -0400

    prun

--- src/Jacinda/Backend/Trans.hs
+++
@@ -1,7 +0,0 @@
-module Jacinda.Backend.Trans ( eToM ) where
-
-import           Jacinda.AST
-import           Jacinda.M
-
-eToM :: E (T K) -> M
-eToM (EApp _ (EApp _ (EApp _ (TB (TyArr _ _ (TyArr _ _ (TyArr _ (TyApp _ (TyB _ TyStream) _) _))) Fold) op) seed) stream) = undefined
