d8831b76fc3073c441070ecf1de5ae7e198d9627
Vanessa McHale <vamchale@gmail.com>
2023-04-08 18:07:08 -0400

    fuse

--- src/Jacinda/Backend/P.hs
+++ src/Jacinda/Backend/P.hs
@@ -4,10 +4,12 @@ import qualified Data.ByteString        as BS
 import           Jacinda.AST
 import           Jacinda.AST.I
 import           Jacinda.Backend.Stream
+import           Jacinda.Fuse
 import           Regex.Rure             (RurePtr)
 
 runJac :: RurePtr -- ^ Record separator
        -> Int
        -> Program (T K)
        -> Either StreamError ([BS.ByteString] -> IO ())
-runJac re i e = fileProcessor re (flushD e) (fst$ib i e)
+runJac re i e = fileProcessor re (flushD e) (fst $ uncurry (flip fuse) $ ib i e)
+-- beta-reduce again?
