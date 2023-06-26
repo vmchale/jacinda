8fdafb582458fcb57d19f632e97536dc6ba89d74
Vanessa McHale <vamchale@gmail.com>
2023-06-26 19:55:59 -0400

    yahhhh

--- src/Jacinda/Backend/P.hs
+++ src/Jacinda/Backend/P.hs
@@ -15,7 +15,7 @@ import           Jacinda.Backend.Parse
 import           Jacinda.Fuse
 import           Jacinda.Regex
 import           Jacinda.Ty.Const
-import           Prettyprinter              (pretty, hardline)
+import           Prettyprinter              (hardline, pretty)
 import           Prettyprinter.Render.Text  (putDoc)
 import           Regex.Rure                 (RurePtr)
 
@@ -32,7 +32,7 @@ instance Exception StreamError where
 
 data EvalError = EmptyFold
                | IndexOutOfBounds Int
-               | InternalCoercionError TB
+               | InternalCoercionError (E (T K)) TB
                deriving (Show)
 
 instance Exception EvalError where
@@ -63,36 +63,37 @@ bsProcess r f u e | (TyApp _ (TyB _ TyStream) _) <- eLoc e =
 
 eStream :: Int -> RurePtr -> E (T K) -> [BS.ByteString] -> [E (T K)]
 eStream i r (EApp _ (UB _ CatMaybes) e) bs                                    = mapMaybe asM$eStream i r e bs
-eStream _ r (Implicit _ e) bs                                                 = zipWith (\fs i -> eB (eCtx fs i) e) [splitBy r b | b <- bs] [1..]
+eStream _ r (Implicit _ e) bs                                                 = zipWith (\fs i -> eB (eCtx fs i) e) [(b, splitBy r b) | b <- bs] [1..]
 eStream _ _ AllColumn{} bs                                                    = mkStr<$>bs
 eStream i r (EApp _ (EApp _ (BB _ MapMaybe) f) e) bs                          = let xs = eStream i r e bs in mapMaybe (\eϵ -> asM (eB id$mapOp i f eϵ)) xs
 eStream i r (EApp (TyApp _ (TyB _ TyStream) (TyB _ TyStr)) (UB _ Dedup) e) bs = let s = eStream i r e bs in mkStr<$>nubOrd(asS<$>s)
-eStream _ r (Guarded _ p e) bs = let bss=splitBy r<$>bs in catMaybes$zipWith (\fs i -> if asB (eB (eCtx fs i) p) then Just (eB (eCtx fs i) e) else Nothing) bss [1..]
+eStream _ r (Guarded _ p e) bs                                                = let bss=(\b -> (b, splitBy r b))<$>bs in catMaybes$zipWith (\fs i -> if asB (eB (eCtx fs i) p) then Just (eB (eCtx fs i) e) else Nothing) bss [1..]
 
 mapOp :: Int -> E (T K) -> E (T K) -> E (T K)
 mapOp i f x | TyArr _ _ cod <- eLoc f = fst (β i (EApp cod f x))
 
 asS :: E (T K) -> BS.ByteString
-asS (StrLit _ s) = s; asS _ = throw (InternalCoercionError TyStr)
+asS (StrLit _ s) = s; asS e = throw (InternalCoercionError e TyStr)
 
 asI :: E (T K) -> Integer
-asI (IntLit _ i) = i; asI _ = throw (InternalCoercionError TyInteger)
+asI (IntLit _ i) = i; asI e = throw (InternalCoercionError e TyInteger)
 
 asR :: E (T K) -> RurePtr
-asR (RegexCompiled r) = r; asR _ = throw (InternalCoercionError TyR)
+asR (RegexCompiled r) = r; asR e = throw (InternalCoercionError e TyR)
 
 asM :: E (T K) -> Maybe (E (T K))
-asM (OptionVal _ e) = e; asM _ = throw (InternalCoercionError TyOption)
+asM (OptionVal _ e) = e; asM e = throw (InternalCoercionError e TyOption)
 
 asB :: E (T K) -> Bool
-asB (BoolLit _ b) = b; asB _ = throw (InternalCoercionError TyBool)
+asB (BoolLit _ b) = b; asB e = throw (InternalCoercionError e TyBool)
 
-eCtx :: V.Vector BS.ByteString -- ^ Line, split by field separator
+eCtx :: (BS.ByteString, V.Vector BS.ByteString) -- ^ Line, split by field separator
      -> Integer -- ^ Line number
      -> E (T K) -> E (T K)
-eCtx fs _ (Field _ i) = mkStr (fs ! (i-1))
-eCtx _ i (NB _ Ix)    = mkI i
-eCtx _ _ e            = e
+eCtx ~(f, _) _ AllField{}  = mkStr f
+eCtx (_, fs) _ (Field _ i) = mkStr (fs ! (i-1))
+eCtx _ i (NB _ Ix)         = mkI i
+eCtx _ _ e                 = e
 
 eB :: (E (T K) -> E (T K)) -> E (T K) -> E (T K)
 eB f (EApp _ (EApp _ (EApp _ (TB _ Captures) s) i) r) =
@@ -101,4 +102,7 @@ eB f (EApp _ (EApp _ (EApp _ (TB _ Captures) s) i) r) =
 eB f (EApp _ (EApp _ (BB (TyArr _ (TyB _ TyInteger) _) Eq) x0) x1) =
     let x0'=asI(eB f x0); x1'=asI(eB f x1)
     in mkB (x0'==x1')
+eB f (EApp _ (EApp _ (BB _ Matches) s) r) =
+    let s'=asS(eB f s); r'=asR(eB f r)
+    in mkB (isMatch' r' s')
 eB f e = f e
