4bd391bd117bc22d4e5c6d310cc0670b28217586
Vanessa McHale <vamchale@gmail.com>
2023-07-26 21:18:38 -0400

    not technically correct whoops

--- src/Jacinda/Fuse.hs
+++ src/Jacinda/Fuse.hs
@@ -34,22 +34,6 @@ fM (EApp t0 (EApp t1 (EApp t2 ho@(TB _ Fold) op) seed) stream) | TyApp (TyB TySt
                 fop=Lam fopT s (Lam popT x (EApp undefined (EApp undefined op sE) (EApp yTy f xE)))
             fM (EApp sTy (EApp undefined (EApp undefined (TB (TyArr fopT (TyArr sTy (TyArr (TyApp (TyB TyStream) xTy) sTy))) Fold) fop) seed) xs)
         _ -> pure (EApp t0 (EApp t1 (EApp t2 ho op) seed) stream')
-fM (EApp t0 (EApp t1 ho@(BB _ Fold1) op) stream) | TyApp (TyB TyStream) _ <- eLoc stream = do
-    stream' <- fM stream
-    case stream' of
-        (EApp _ (EApp _ (BB _ Filter) p) xs) -> do
-            let opT@(TyArr xT popT) = eLoc op
-            s <- nN "s" xT; x <- nN "x" xT
-            let sE=Var xT s; xE=Var xT x
-                fop=Lam opT s (Lam popT x (Cond xT (EApp tyB p xE) (EApp xT (EApp popT op sE) xE) sE))
-            pure (EApp t0 (EApp t1 ho fop) xs)
-        (Guarded t p e) -> do
-            let opT@(TyArr xT popT) = eLoc op
-            s <- nN "s" xT; x <- nN "x" xT
-            let sE=Var xT s; xE=Var xT x
-                fop=Lam opT s (Lam popT x (Cond xT p (EApp xT (EApp popT op sE) xE) sE))
-            pure (EApp t0 (EApp t1 ho fop) (Implicit t e))
-        _ -> pure (EApp t0 (EApp t1 ho op) stream')
 fM (Tup t es) = Tup t <$> traverse fM es
 fM (EApp t e0 e1) = EApp t <$> fM e0 <*> fM e1
 fM (Lam t n e) = Lam t n <$> fM e
