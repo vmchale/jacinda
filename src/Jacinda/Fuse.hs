a12ef1f38793cbaaff811ed04e8574f6f822b576
Vanessa McHale <vamchale@gmail.com>
2023-08-18 19:30:42 -0400

    1

--- src/Jacinda/Fuse.hs
+++ src/Jacinda/Fuse.hs
@@ -10,6 +10,9 @@ import           Ty.Const
 fuse :: Int -> E T -> (E T, Int)
 fuse i = flip runState i.fM
 
+-- fold1: needs a special "form" for fold1-of-map (pick seed through map)
+-- also "filter-of-fold1" b/c we need to pick a seed that isn't filtered.
+
 fM :: E T -> M (E T)
 fM (EApp t0 (EApp t1 (EApp t2 ho@(TB _ Fold) op) seed) stream) | TyApp (TyB TyStream) _ <- eLoc stream = do
     stream' <- fM stream
@@ -24,7 +27,7 @@ fM (EApp t0 (EApp t1 (EApp t2 ho@(TB _ Fold) op) seed) stream) | TyApp (TyB TySt
             s <- nN "seed" sTy; x <- nN "x" xTy
             let sE=Var sTy s; xE=Var xTy x
             let fop=Lam opTy s (Lam popTy x (Cond sTy p (EApp sTy (EApp popTy op sE) xE) sE)) in pure (EApp t0 (EApp t1 (EApp t2 ho fop) seed) (Implicit t e))
-            -- FIXME: evaluates e (could be exception)
+            -- FIXME: does this evaluate e? (could be exception)
         (EApp _ (EApp _ (BB _ Map) f) xs) -> do
             let (TyArr xTy yTy) = eLoc f
                 (TyArr sTy _) = eLoc op
@@ -33,6 +36,19 @@ fM (EApp t0 (EApp t1 (EApp t2 ho@(TB _ Fold) op) seed) stream) | TyApp (TyB TySt
                 popT=TyArr xTy sTy; fopT=TyArr sTy popT
                 fop=Lam fopT s (Lam popT x (EApp undefined (EApp undefined op sE) (EApp yTy f xE)))
             fM (EApp sTy (EApp undefined (EApp undefined (TB (TyArr fopT (TyArr sTy (TyArr (TyApp (TyB TyStream) xTy) sTy))) Fold) fop) seed) xs)
+        (EApp _ (EApp _ (BB _ MapMaybe) f) xs) -> do
+            undefined
+        (EApp _ (UB _ CatMaybes) xs) -> do
+            -- op | seed (.? stream') -> [x `op` (option x id y)] | seed stream'
+            let TyArr _ xTy=eLoc op
+                xMT=tyOpt xTy
+                sTy=eLoc seed
+                opTy=TyArr sTy (TyArr xMT sTy)
+            s <- nN "seed" sTy; x <- nN "x" xTy
+            let sE=Var sTy s; xE=Var xMT x
+                popT=TyArr xMT sTy; fopT=TyArr sTy popT
+                fop=Lam fopT s (Lam popT x (EApp undefined (EApp undefined op sE) undefined))
+            pure (EApp sTy (EApp undefined (EApp undefined (TB (TyArr fopT (TyArr sTy (TyArr (TyApp (TyB TyStream) xMT) sTy))) Fold) fop) seed) xs)
         _ -> pure (EApp t0 (EApp t1 (EApp t2 ho op) seed) stream')
 fM (Tup t es) = Tup t <$> traverse fM es
 fM (EApp t e0 e1) = EApp t <$> fM e0 <*> fM e1
