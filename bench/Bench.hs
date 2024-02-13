677fa8b28f8a584baa57d7eb495c14a8481c82b6
Vanessa McHale <vamchale@gmail.com>
2024-02-13 17:34:49 -0500

    path3

--- bench/Bench.hs
+++ bench/Bench.hs
@@ -3,16 +3,17 @@
 module Main (main) where
 
 import           A
-import           Control.DeepSeq    (NFData (..))
+import           Control.DeepSeq      (NFData (..))
 import           Criterion.Main
-import qualified Data.Text.IO       as TIO
+import qualified Data.Text.IO         as TIO
 import           File
-import           System.IO.Silently (silence)
+import           System.IO.Silently   (silence)
 
 main :: IO ()
 main =
     defaultMain [ bgroup "eval"
                       [ bench "exprEval" $ nf exprEval "[x+' '+y]|'' split '01-23-1987' /-/"
+                      , bench "path" $ nfIO (silence $ runOnFile [] "{|[x+'\\n'+y]|>`$}" (Just ":") Nothing "bench/data/PATH")
                       , bench "runOnFile" $ nfIO (silence $ runOnFile [] "(+)|0 {%/Bloom/}{1}" Nothing Nothing "bench/data/ulysses.txt")
                       , bench "runOnFile" $ nfIO (silence $ do { contents <- TIO.readFile "examples/wc.jac" ; runOnFile [] contents Nothing Nothing "bench/data/ulysses.txt" })
                       , bench "runOnFile" $ nfIO (silence $ do { contents <- TIO.readFile "examples/span2.jac" ; runOnFile [] contents Nothing Nothing "bench/data/span.txt" })
