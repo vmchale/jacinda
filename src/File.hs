a010793c870fc1399de194357157365a9abcc861
Vanessa McHale <vamchale@gmail.com>
2024-06-04 09:24:29 -0400

    for ill-formed data

--- src/File.hs
+++ src/File.hs
@@ -11,6 +11,7 @@ import           A.I
 import           Control.Applicative        ((<|>))
 import           Control.Exception          (Exception, throw, throwIO)
 import           Control.Monad.IO.Class     (liftIO)
+import           Control.Monad.ST.Strict    as ST
 import           Control.Monad.State.Strict (StateT, get, put, runState, runStateT)
 import           Control.Recursion          (cata, embed)
 import           Data.Bifunctor             (second)
@@ -24,12 +25,13 @@ import           Data.Text.Encoding         (encodeUtf8)
 import qualified Data.Text.IO               as TIO
 import           Data.Tuple                 (swap)
 import qualified Data.Vector                as V
+import qualified Data.Vector.Mutable        as MV
 import           Include
 import           Jacinda.Backend.Const
 import           Jacinda.Backend.T
 import           Jacinda.Check.Field
 import           Jacinda.Regex
-import           L
+import           L                          hiding (ix)
 import           Parser
 import           Parser.Rw
 import           R
@@ -37,22 +39,22 @@ import           Regex.Rure                 (RurePtr)
 import           System.IO                  (stdin)
 import           Text.CSV.Lazy.ByteString   (CSVField (..), parseCSV)
 import           Ty
-import Control.Monad.ST.Strict as ST
-import Control.Monad.ST.Strict (runST)
-import qualified Data.Vector.Mutable as MV
 
 m'Vec :: Int -> [a] -> V.Vector a
 m'Vec n xs = runST $ do
     v <- MV.unsafeNew n
+    let go ix (x:xsϵ) | ix < n = MV.write v ix x *> go (ix+1) xsϵ
+                      | otherwise = MV.grow v 1 *> MV.write v ix x *> go (ix+1) xsϵ
+        go _ [] = pure ()
+    go 0 xs
     V.unsafeFreeze v
 
 csvCtx :: BSL.ByteString -> [LineCtx]
 csvCtx = go Nothing . parseCSV where
-    go _ []           = []
-    go _ (Left err:_) = error (show err)
+    go _ []                  = []
+    go _ (Left err:_)        = error (show err)
     -- TODO: re-csv it?
-    -- FIXME: check n as fs are written (right now it's just a guess)
-    go (Just n) (Right r:rs) = let fs=mB<$>r in (fold fs, V.fromListN n fs, fromIntegral (line r)):go (Just n) rs
+    go (Just n) (Right r:rs) = let fs=mB<$>r in (fold fs, m'Vec n fs, fromIntegral (line r)):go (Just n) rs
     go Nothing (Right r:rs)  = let fs=mB<$>r; n=length fs in (fold fs, V.fromListN n fs, fromIntegral (line r)):go (Just n) rs
     mB f@CSVField{} = BSL.toStrict (csvFieldContent f)
     mB f            = error (show f)
