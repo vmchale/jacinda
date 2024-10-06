5f967479dea9f3215e4fbd356346b66480061591
Vanessa McHale <vamchale@gmail.com>
2024-10-06 13:57:12 -0400

    fmt

--- src/R.hs
+++ src/R.hs
@@ -88,21 +88,21 @@ mkLam ns e = foldr (\n -> Lam (loc n) n) e ns
 
 hasY :: E a -> Bool
 hasY = g where
-    g (ResVar _ Y)                 = True
-    g (Tup _ es)                   = any g es
-    g (Rec _ es)                   = any (g.snd) es
-    g (OptionVal _ (Just e))       = g e
-    g (EApp _ e0 e1)               = g e0 || g e1
-    g Dfn{}                        = error "nested dfns not yet implemented"
-    g (Let _ (_, be) e)            = g e || g be
-    g (Lam _ _ e)                  = g e
-    g (Paren _ e)                  = g e
-    g (Guarded _ p e)              = g p || g e
-    g (Implicit _ e)               = g e
-    g (Arr _ es)                   = any g es
-    g (Anchor _ es)                = any g es
-    g (Cond _ p e0 e1)             = g e0 || g e1 || g p
-    g _                            = False
+    g (ResVar _ Y)           = True
+    g (Tup _ es)             = any g es
+    g (Rec _ es)             = any (g.snd) es
+    g (OptionVal _ (Just e)) = g e
+    g (EApp _ e0 e1)         = g e0 || g e1
+    g Dfn{}                  = error "nested dfns not yet implemented"
+    g (Let _ (_, be) e)      = g e || g be
+    g (Lam _ _ e)            = g e
+    g (Paren _ e)            = g e
+    g (Guarded _ p e)        = g p || g e
+    g (Implicit _ e)         = g e
+    g (Arr _ es)             = any g es
+    g (Anchor _ es)          = any g es
+    g (Cond _ p e0 e1)       = g e0 || g e1 || g p
+    g _                      = False
 
 replaceXY :: (a -> Nm a) -- ^ @x@
           -> (a -> Nm a) -- ^ @y@
