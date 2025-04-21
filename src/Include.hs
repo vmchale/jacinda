47b103a4708124515ee0845b41cf431f80305590
Vanessa McHale <vamchale@gmail.com>
2025-04-21 09:11:55 -0400

    bug

--- src/Include.hs
+++ src/Include.hs
@@ -2,13 +2,14 @@
 
 module Include ( defaultIncludes, resolveImport ) where
 
-import           Control.Exception  (Exception, throwIO)
-import           Control.Monad      (filterM)
-import           Data.List.Split    (splitWhen)
-import           Paths_jacinda      (getDataDir)
-import           System.Directory   (doesDirectoryExist, doesFileExist, getCurrentDirectory)
-import           System.Environment (lookupEnv)
-import           System.FilePath    ((</>))
+import           Control.Exception         (Exception, throwIO)
+import           Control.Monad             (filterM, (<=<))
+import           Data.Containers.ListUtils (nubOrd)
+import           Data.List.Split           (splitWhen)
+import           Paths_jacinda             (getDataDir)
+import           System.Directory          (canonicalizePath, doesDirectoryExist, doesFileExist, getCurrentDirectory)
+import           System.Environment        (lookupEnv)
+import           System.FilePath           ((</>))
 
 data ImportError = FileNotFound !FilePath ![FilePath] | AmbiguousInclude ![FilePath] deriving (Show)
 
@@ -30,6 +31,8 @@ jacPath = maybe [] splitEnv <$> lookupEnv "JAC_PATH"
     splitEnv = splitWhen (== ':')
 
 resolveImport :: [FilePath] -> FilePath -> IO FilePath
-resolveImport incl fp =
-    (\case [] -> throwIO $ FileNotFound fp incl; [fp] -> pure fp; fs -> throwIO $ AmbiguousInclude fs)
-        =<< (filterM (doesFileExist . (</> fp)) $ incl)
+resolveImport incl fp = ($incl) $
+    (\case [] -> throwIO $ FileNotFound fp incl; [src] -> pure (src</>fp); fs -> throwIO $ AmbiguousInclude fs)
+    . nubOrd
+        <=< traverse canonicalizePath
+        <=< (filterM (doesFileExist . (</> fp)))
